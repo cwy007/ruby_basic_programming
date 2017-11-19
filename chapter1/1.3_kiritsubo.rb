32e36d350d256562a6a6470dfaf91fcec8f474e7
cwy007 <chanweiyan007@gmail.com>
2017-11-19 15:08:18 +0800

    chapter1 end

---
+++ chapter1/1.3_kiritsubo.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+print "话说某个朝代，后宫妃嫔甚多，\n"
+print "其中有一宫女，出身并不高贵，却蒙圣恩宠爱。\n"
+
+=begin
+ruby -E UTF-8 脚本文件名
+
+irb -E UTF-8
+=end
---
+++ chapter1/1.6_bigger_smaller.rb
@@ -0,0 +1,15 @@
+a = 20
+if a >= 10 then
+  print "bigger\n"
+end
+if a <= 9 then
+  print "smaller\n"
+end
+
+
+a = 20
+if a >= 10 
+  print "bigger\n"
+else a <= 9
+  print "smaller\n"
+end
